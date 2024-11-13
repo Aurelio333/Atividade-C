d663a495dbe2892e1561741c05b3123bb2275b2b
aurelio333 <eng.antonioaurelio@gmail.com>
2024-11-13 11:07:05 -0300

    initial commit

---
+++ Ex5.C
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    char letra;
+
+    printf("Insira um caractere: ");
+    letra = getchar();
+    printf("Você digitou: '%c'", letra);
+}
---
+++ ex1.C
@@ -0,0 +1,7 @@
+#include <stdio.h>
+ 
+int main()
+{
+    printf("Aurelio o cara \n");
+    return 0;
+}
